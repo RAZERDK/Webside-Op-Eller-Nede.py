842f2d2b998d2e62371ccdcce42575efa862a3df
RΛZΞRDK <74677190+RAZERDK@users.noreply.github.com>
2020-11-18 19:42:33 +0100

    Add files via upload

---
+++ Webside Op Eller Nede tjekker.py
@@ -0,0 +1,43 @@
+import os
+import time
+
+hostnames = [
+    'Github.com',
+    'Heroku.com',
+    'discord.com',
+    'Rasmus er Fucking sejere end dig.XD',
+    #Add More by your self
+]
+for hostname in hostnames:
+    response = os.system('ping  ' + hostname)
+
+    if response == 0:
+        print (hostname, '')
+        print (hostname, '')
+        print (hostname, '')
+        print (hostname, '')
+        print (hostname, '')
+        print (hostname, '')
+        print (hostname, '')
+        print (hostname, '')
+        print (hostname, 'Er Oppe og Køre')
+    else:
+        print (hostname, '')
+        print (hostname, '')
+        print (hostname, '')
+        print (hostname, '')
+        print (hostname, '')
+        print (hostname, '')
+        print (hostname, '')
+        print (hostname, ' er Nede Ligesom UniLogin Hver Dag')     
+    #Venter På Response
+        print ('')
+        print ('')
+        print ('')
+        print ('')
+        print ('')
+        print ('')
+        print("Alle Angivet Websider har fåedt Tjeket Status")
+        #Venter 100 Sekunder Og Lukker Lortet
+        time.sleep(100.1)
+        print("Lukker Nu")
